15affa531883650b03ddbc3842e7d306c66a31fd
Ankit Mittal <111688303+ankitmittal4@users.noreply.github.com>
2022-12-26 21:06:09 +0530

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,4 @@
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* llist, int data) {
+    DoublyLinkedListNode* head = llist;
+    DoublyLinkedListNode* nodeToInsert = new DoublyLinkedListNode(data);
+    
